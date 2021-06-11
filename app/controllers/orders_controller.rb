b8e45874792e9c3ba3f07c48abbec16f138d1181
shigemasa-suzuki <shigemasa.suzuki@gmail.com>
2021-06-11 21:28:31 +0900

    修正しました.1

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,9 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!, only: :index
   before_action :set_item, only: [:index,:create]
-  before_action :sold_out_item, only: [:index]
+  before_action :move_to_index,only: [:edit,:index]
+
+
   def index
     @order_address = OrderAddress.new
   end
@@ -36,8 +38,10 @@ class OrdersController < ApplicationController
       )
     end
 
-    def sold_out_item
-      redirect_to root_path if @item.order.present?
-     end
 
+     def move_to_index
+      if current_user.id != @item.user_id || @item.order.present? 
+         redirect_to action: :index
+      end
+    end
 end
